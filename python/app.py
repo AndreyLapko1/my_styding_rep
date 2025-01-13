6893911d362340f9668bb35d99d2f3fb1883daee
AndreyLapko1 <andreycgo765@gmail.com>
2025-01-13 18:33:30 +0300

    update app.py 2025/01/13/18:33

--- python/app.py
+++ python/app.py
@@ -1,152 +1,14 @@
-import mysql.connector
-import os
-from dotenv import load_dotenv
+# import mysql.connector
+# import os
+# from dotenv import load_dotenv
+# import re
 from telebot.types import InlineKeyboardButton, InlineKeyboardMarkup
-import re
+from database import QueryDatabase, Database
 
 
 
 
-class QueryDatabase:
-    def __init__(self):
-        load_dotenv()
-        self.connection = mysql.connector.connect(
-            host=os.getenv("host_write"),
-            user=os.getenv("user_write"),
-            password=os.getenv("password_write"),
-            database=os.getenv("db_write"),
-        )
-        self.cursor = self.connection.cursor()
 
-
-    def tracker(self, filter_, pattern):
-        try:
-            self.cursor.execute(f'insert into requests (search_by, title) values (%s, %s)', (filter_, pattern))
-            self.connection.commit()
-        except mysql.connector.Error as err:
-            print(f'Something went wrong ', err)
-
-
-    def show_most_common(self):
-        query = '''
-                        SELECT title, COUNT(*) AS counter FROM `290724-ptm_fd_Andrey_Lapko`.requests
-                        GROUP BY title
-                        ORDER BY counter DESC
-                        LIMIT 5'''
-        self.cursor.execute(query)
-        return self.cursor.fetchall()
-
-    def close(self):
-        self.connection.close()
-
-
-
-#sakila
-class Database:
-    def __init__(self):
-        load_dotenv()
-        try:
-            self.connection = mysql.connector.connect(
-                host=os.getenv("host"),
-                user=os.getenv("user"),
-                password=os.getenv("password"),
-                database=os.getenv("database"),
-            )
-            self.cursor = self.connection.cursor()
-            print("Connection established")
-        except mysql.connector.Error as err:
-            print(f'Something went wrong ', err)
-
-    def show_categories(self):
-        query = 'select * from category'
-        self.cursor.execute(query)
-        return self.cursor.fetchall()
-
-    def search_by_keyword(self, keyword, limit=10, offset=0):
-        try:
-            base_query = f'''
-                            SELECT distinct f.title
-                            FROM sakila.film f
-                            join film_actor a
-                            on f.film_id = a.film_id
-                            join actor act
-                            on a.actor_id = act.actor_id
-                            where lower(act.first_name) LIKE %s
-                            or lower(act.last_name) like %s
-                            or lower(f.title) like %s
-                            LIMIT {limit} OFFSET {offset}
-                '''
-            self.cursor.execute(base_query, (keyword, keyword, keyword))
-            return self.cursor.fetchall()
-        except mysql.connector.Error as err:
-            print(f'Something went wrong ', err)
-
-
-    def search_by_category_year(self, year, category):
-        base_query = '''
-                         SELECT f.title, cat.name FROM sakila.film_category fcat
-                         inner join category cat
-                         on fcat.category_id = cat.category_id
-                         inner join film f
-                         on f.film_id = fcat.film_id
-                         where f.release_year = %s and cat.name = %s
-
-                                            '''
-        self.cursor.execute(base_query, (year, category))
-        return self.cursor.fetchall()
-
-    def search_by_category(self, genre_name, limit=10, offset=0):
-        try:
-            base_query = f'''  
-                                            select f.title FROM sakila.film_category fc
-                                                    inner join category c
-                                                    on c.category_id = fc.category_id
-                                                    inner join film f
-                                                    on f.film_id = fc.film_id
-                                                    where c.name = %s
-                                                    LIMIT {limit} OFFSET {offset}
-                                                '''
-            self.cursor.execute(base_query, (genre_name,))
-            return self.cursor.fetchall()
-        except mysql.connector.Error as err:
-            print(f'Something went wrong ', err)
-
-
-
-    def search_by_year(self, year, limit=10, offset=0):
-        try:
-            base_query = f'''
-            SELECT title FROM sakila.film
-            where release_year = %s
-            LIMIT {limit} OFFSET {offset}
-                                            '''
-            self.cursor.execute(base_query, (year,))
-            return self.cursor.fetchall()
-        except mysql.connector.Error as err:
-            print(f'Something went wrong ', err)
-
-
-    def search_info(self, film_name):
-        try:
-            base_query = '''
-            SELECT f.title, c.name, description, release_year, lng.name, rental_rate FROM sakila.film f
-            inner join film_category fc
-            on f.film_id = fc.film_id
-            inner join category c
-            on fc.category_id = c.category_id
-            inner join language lng
-            on f.language_id = lng.language_id
-            where f.title = %s
-            '''
-            self.cursor.execute(base_query, (film_name,))
-            return self.cursor.fetchall()
-        except mysql.connector.Error as err:
-            print(f'Something went wrong ', err)
-
-
-
-    def close(self):
-        self.connection.close()
 
 class App:
     def __init__(self, bot):
@@ -158,16 +20,7 @@ class App:
 
 
     def display(self, chat_id, results=None, pattern=None,  offset=0, more=False, func=None):
-        # if query:
-        #     if re.findall(pattern_regex, query, flags=re.DOTALL):
-        #         print(self.query)
-        #         match = re.findall(pattern_regex, query, re.DOTALL)
-        #         cleaned_query = match[0]
-        #         self.query = cleaned_query
-        #     else:
-        #         self.query = query
-
-
+        keyboard = InlineKeyboardMarkup()
         if more:
             method = getattr(self.db, func)
             new_results = method(pattern, offset=offset)
@@ -176,12 +29,13 @@ class App:
                 self.display(chat_id, results=new_results, pattern=pattern, func=method.__name__, offset=offset)
                 return
             else:
-                self.bot.send_message(chat_id, "No more results.")
+                keyboard.add(InlineKeyboardButton(text=f'Return', callback_data=f'return'))
+                self.bot.send_message(chat_id, "No more results.", reply_markup=keyboard)
                 return
 
         if isinstance(results, list) or isinstance(results, tuple):
             print(results[:10])
-            keyboard = InlineKeyboardMarkup()
+
             for row in results[:10]:
                 print(row[0])
                 keyboard.add(InlineKeyboardButton(text=f'{row[0].capitalize()}', callback_data=f'film_{row[0]}'))
