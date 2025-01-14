32279cac077dc1f62e56ee922b640ccbad5edfc0
AndreyLapko1 <andreycgo765@gmail.com>
2025-01-14 23:25:33 +0300

    add images.py

---
+++ python/movie_telebot_project/images.py
@@ -0,0 +1,12 @@
+genre_images = {
+    "Action": ["C:/Users/andre/Desktop/img/action.jpg", "C:/Users/andre/Desktop/img/action2.webp"],
+    "Comedy": ["C:/Users/andre/Desktop/img/comedy.jpg", "C:/Users/andre/Desktop/img/comedy2.jpg"],
+    "Drama": ["C:/Users/andre/Desktop/img/drama.webp", "C:/Users/andre/Desktop/img/drama2.jpg"],
+    "Horror": ["C:/Users/andre/Desktop/img/horror.jpg", "C:/Users/andre/Desktop/img/horror2.webp"],
+    "Sci-Fi": ["C:/Users/andre/Desktop/img/scifi.jpg","C:/Users/andre/Desktop/img/scifi2.jpg"],
+    "Travel": ["C:/Users/andre/Desktop/img/travel.jpg",],
+    "Sports": ["C:/Users/andre/Desktop/img/sport.jpg", "C:/Users/andre/Desktop/img/sport2.jpg"],
+    "New": ["C:/Users/andre/Desktop/img/new.jpg", "C:/Users/andre/Desktop/img/new2.webp"],
+    "Music": ["C:/Users/andre/Desktop/img/music.jpg", ],
+    "Games": ["C:/Users/andre/Desktop/img/games.jpg", "C:/Users/andre/Desktop/img/games2.jpg"],
+}
